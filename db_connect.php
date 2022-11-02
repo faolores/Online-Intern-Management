f91c3079136f2766c2d7b3577840767aa0952059
faolores <faolores@student.apc.edu.ph>
2022-11-02 10:07:11 +0800

    database

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','evaluation_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli($_ENV['DB_HOST'] ?? 'localhost',$_ENV['DB_USER'] ?? 'root',$_ENV['DB_PASSWORD'] ?? '',$_ENV['DB_DATABASE'] ?? 'evaluation_db')or die("Could not connect to mysql".mysqli_error($con));
